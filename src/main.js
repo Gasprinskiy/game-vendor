301268a9f47148628b8186d4c95904aa5de6f4e9
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-03-11 21:52:56 +0500

    app main js file

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import router from './router'
+import store from './store'
 import "@fontsource/inter"
-createApp(App).use(Quasar, quasarUserOptions).mount('#app')
+
+createApp(App)
+.use(Quasar, quasarUserOptions)
+.use(store)
+.use(router)
+.mount('#app')
