0139d58b89d3b750ee2d337a23054a93e1378ffb
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-03-11 21:52:35 +0500

    fake data base

---
+++ src/db.js
@@ -0,0 +1,151 @@
+
+
+export default {
+    games: [        
+            {
+                id: 1,
+                img: 'image-1.jpg',
+                status: false, 
+                title: 'Saints Row', 
+                category: 'Action RPG', 
+                rating: {votes: 0, grade: 0}, 
+                description:'Platinum Edition', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 2,
+                img: 'image-2.jpg',
+                status: false,  
+                title: 'Paladins', 
+                category: 'Multiplayer RPG', 
+                rating: {votes: 1820, grade: 4}, 
+                description:'Get the Captain Price Operator included with purchase', 
+                priceInfo: {price: 0}
+            },
+            {
+                id: 3,
+                img: 'image-3.jpg',
+                status: false,  
+                title: 'Far Cry 6', 
+                category: 'Action', 
+                rating: {votes: 2880, grade: 4}, 
+                description:'In-Game Content', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 4,
+                img: 'image-4.jpg',
+                status: false, 
+                title: 'Call of Duty: Warzone', 
+                category: 'Shooter', 
+                rating: {votes: 18922, grade: 3}, 
+                description:'Get the Captain Price Operator included with purchase', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 5,
+                img: 'image-5.jpg',
+                status: false,  
+                title: 'World of Warcraft: Shadowlands', 
+                category: 'Action', 
+                rating: {votes: 299, grade: 3}, 
+                description:'Includes access to World of Warcraft, Burning Crusade Classic, and WoW Classic!', 
+                priceInfo: {price: 0}
+            },
+            {
+                id: 6,
+                img: 'image-6.jpg',
+                status: false,  
+                title: 'Fortnite', 
+                category: 'Shooter', 
+                rating: {votes: 9200, grade: 5}, 
+                description:'Get the Captain Price Operator included with purchase', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 7,
+                img: 'image-7.jpg',
+                status: false, 
+                title: 'Horizon: Forbidden West', 
+                category: 'Action RPG', 
+                rating: {votes: 9195, grade: 5}, 
+                description:'Platinum Edition', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 8,
+                img: 'image-8.jpg',
+                status: false,  
+                title: 'Need for Speed: Heat', 
+                category: 'Action', 
+                rating: {votes: 2911, grade: 1}, 
+                description:'Includes access to World of Warcraft, Burning Crusade Classic, and WoW Classic!', 
+                priceInfo: {price: 650}
+            },
+            {
+                id: 9,
+                img: 'image-9.jpg',
+                status: false, 
+                title: 'Spider-Man: Miles Morales', 
+                category: 'Action', 
+                rating: {votes: 920, grade: 3}, 
+                description:'Get the Captain Price Operator included with purchase', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 10,
+                img: 'image-10.jpg',
+                status: false, 
+                title: 'Witcher 3: Wild Hunt', 
+                category: 'Card Game', 
+                rating: {votes: 299890, grade: 5}, 
+                description:'Will only be usable in the new Classic Format when it is released', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 11,
+                img: 'image-11.jpg',
+                status: false, 
+                title: 'Guardians of the Galaxy', 
+                category: 'Shooter', 
+                rating: {votes: 29, grade: 3}, 
+                description:'In-Game Content', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 12,
+                img: 'image-12.jpg',
+                status: false, 
+                title: 'Crash Team Racing', 
+                category: 'Shooter', 
+                rating: {votes: 1820, grade: 4}, 
+                description:'Get the Captain Price Operator included with purchase', 
+                priceInfo: {price: 1299, discount: 50}
+            },
+            {
+                id: 13,
+                img: 'image-13.jpg',
+                status: false, 
+                title: 'Spider-Man', 
+                category: 'Multiplayer RPG', 
+                rating: {votes: 1820, grade: 4}, 
+                description:'Includes access to World of Warcraft, Burning Crusade Classic, and WoW Classic!', 
+                priceInfo: {price: 650}
+            },
+            {
+                id: 14,
+                status: false, 
+                title: 'Dragon Age 5', 
+                category: 'Action RPG', 
+                rating: {votes: 0, grade: 0}, 
+                description:'Non released', 
+                priceInfo: {price: 'unset'}
+            }
+    ], 
+    faq: [
+        {question: 'How does work?', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset. A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset.', list: ['motion sensing device,', 'controller,', 'keyboard,', 'joystick.'], bottom:{title: 'Visual feedback.', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback.'}},
+        {question: 'What is a game?', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset. A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset.', list: ['motion sensing device,', 'controller,', 'keyboard,', 'joystick.'], bottom:{title: 'Visual feedback.', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback.'}},
+        {question: 'Is there a free trial available?', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset. A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset.', list: ['motion sensing device,', 'controller,', 'keyboard,', 'joystick.'], bottom:{title: 'Visual feedback.', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback.'}},
+        {question: 'How does billing work?', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset. A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback. This feedback is shown on a video display device, such as a TV set, monitor, touchscreen, or virtual reality headset.', list: ['motion sensing device,', 'controller,', 'keyboard,', 'joystick.'], bottom:{title: 'Visual feedback.', body: 'A video game or computer game is an electronic game that involves interaction with a user interface or input device – such as a joystick, controller, keyboard, or motion sensing device – to generate visual feedback.'}},
+    ]        
+}
